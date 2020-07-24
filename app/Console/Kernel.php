8c195034065a612def652844de0164e5621337d0
Sergey <sergey86@gmail.com>
2020-07-24 15:29:02 +0300

    disable cron task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('parser:schedule')->daily();
+//        $schedule->command('parser:schedule')->daily();
         // $schedule->command('inspire')->hourly();
     }
 
