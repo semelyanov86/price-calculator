c2e09e47a8161704abec3155537d3831c9ec00f8
Sergey <sergey86@gmail.com>
2020-07-27 15:57:35 +0300

    Add cron tasks for parser

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command('parser:schedule')->daily();
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('parser:schedule')->daily();
+        $schedule->command('parser:init')->everyTenMinutes();
     }
 
     /**
